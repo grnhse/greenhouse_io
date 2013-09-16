64d4852f8e534c2c0c575db1e1c01e72086e9d3b
Adrian Bautista <adrianbautista8@gmail.com>
2013-09-16 15:15:01 -0400

    basic error object specs

--- spec/greenhouse/error_spec.rb
+++ spec/greenhouse/error_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Greenhouse::Error do
-  pending
+
+  describe "#inspect" do
+
+    context "when http error code is present" do
+      it "creates an error with the code" do
+        http_error = Greenhouse::Error.new(nil, 404)
+        expect(http_error.code).to eq(404)
+        expect(http_error.inspect).to eq("Greenhouse::Error: 404 response from server")
+      end
+    end
+
+    context "when its an internal gem error" do
+      it "creates an error with a message" do
+        gem_error = Greenhouse::Error.new("organization can't be blank", nil)
+        expect(gem_error.message).to eq("organization can't be blank")
+        expect(gem_error.inspect).to eq("Greenhouse::Error: organization can't be blank")
+      end
+    end
+
+  end
+
 end
