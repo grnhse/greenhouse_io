76bb0aaae82b2c35855090dc1a16bcecf1499a5f
Adrian Bautista <adrianbautista8@gmail.com>
2014-04-20 19:53:50 -0400

    Update rspec for errors

--- spec/greenhouse/error_spec.rb
+++ spec/greenhouse/error_spec.rb
@@ -8,7 +8,6 @@ describe GreenhouseIo::Error do
       it "creates an error with the code" do
         http_error = GreenhouseIo::Error.new(nil, 404)
         expect(http_error.code).to eq(404)
-        expect(http_error.inspect).to eq("GreenhouseIo::Error: 404 response from server")
       end
     end
 
@@ -16,7 +15,6 @@ describe GreenhouseIo::Error do
       it "creates an error with a message" do
         gem_error = GreenhouseIo::Error.new("organization can't be blank", nil)
         expect(gem_error.message).to eq("organization can't be blank")
-        expect(gem_error.inspect).to eq("GreenhouseIo::Error: organization can't be blank")
       end
     end
 
