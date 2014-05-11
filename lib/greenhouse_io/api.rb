380ad8daf3aa43023093e2e07fe5f4e832a6ee0f
Adrian Bautista <adrianbautista8@gmail.com>
2014-05-11 11:39:47 -0400

    Add client class for harvest api

--- lib/greenhouse_io/api.rb
+++ lib/greenhouse_io/api.rb
@@ -1,7 +1,7 @@
 module GreenhouseIo
   module API
-    def get_response_hash(url, query = nil)
-      response = self.class.get(url, query: query)
+    def get_response_hash(url, query = nil, basic_auth = nil)
+      response = self.class.get(url, query: query, basic_auth: basic_auth)
       if response.code == 200
         MultiJson.load(response.body, :symbolize_keys => true)
       else
--- lib/greenhouse_io/api/client.rb
+++ lib/greenhouse_io/api/client.rb
@@ -8,56 +8,56 @@ module GreenhouseIo
     attr_accessor :api_token
     base_uri 'https://harvest.greenhouse.io/v1'
 
-    def initialize(api_token = ENV['GREENHOUSE_API_TOKEN'])
-      @api_token = api_token
+    def initialize(api_token = nil)
+      @api_token = api_token || ENV['GREENHOUSE_API_TOKEN']
     end
 
     def offices(id = nil, options = {})
-      get_response_hash "/offices#{path_id(id)}", permitted_options(options)
+      get_response_hash "/offices#{path_id(id)}", permitted_options(options), basic_auth
     end
 
     def departments(id = nil, options = {})
-      get_response_hash "/departments#{path_id(id)}", permitted_options(options)
+      get_response_hash "/departments#{path_id(id)}", permitted_options(options), basic_auth
     end
 
     def candidates(id = nil, options = {})
-      get_response_hash "/candidates#{path_id(id)}", permitted_options(options)
+      get_response_hash "/candidates#{path_id(id)}", permitted_options(options), basic_auth
     end
 
     def activity_feed(id, options = {})
-      get_response_hash "/candidates/#{id}/activity_feed", permitted_options(options)
+      get_response_hash "/candidates/#{id}/activity_feed", permitted_options(options), basic_auth
     end
 
     def applications(id = nil, options = {})
-      get_response_hash "/applications#{path_id(id)}", permitted_options(options)
+      get_response_hash "/applications#{path_id(id)}", permitted_options(options), basic_auth
     end
 
     def scorecards(id, options = {})
-      get_response_hash "/applications/#{id}/scorecards", permitted_options(options)
+      get_response_hash "/applications/#{id}/scorecards", permitted_options(options), basic_auth
     end
 
     def scheduled_interviews(id, options = {})
-      get_response_hash "/applications/#{id}/scheduled_interviews", permitted_options(options)
+      get_response_hash "/applications/#{id}/scheduled_interviews", permitted_options(options), basic_auth
     end
 
     def jobs(id = nil, options = {})
-      get_response_hash "/jobs#{path_id(id)}", permitted_options(options)
+      get_response_hash "/jobs#{path_id(id)}", permitted_options(options), basic_auth
     end
 
     def stages(id, options = {})
-      get_response_hash "/jobs/#{id}/stages", permitted_options(options)
+      get_response_hash "/jobs/#{id}/stages", permitted_options(options), basic_auth
     end
 
     def job_post(id, options = {})
-      get_response_hash "/jobs/#{id}/job_post", permitted_options(options)
+      get_response_hash "/jobs/#{id}/job_post", permitted_options(options), basic_auth
     end
 
     def users(id = nil, options = {})
-      get_response_hash "/users#{path_id(id)}", permitted_options(options)
+      get_response_hash "/users#{path_id(id)}", permitted_options(options), basic_auth
     end
 
     def sources(id = nil, options = {})
-      get_response_hash "/sources#{path_id(id)}", permitted_options(options)
+      get_response_hash "/sources#{path_id(id)}", permitted_options(options), basic_auth
     end
 
     private
