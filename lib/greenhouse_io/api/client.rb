60055a07d8fceac53c2e089d68cb11162bb8ac0d
Lien Van Den Steen <lienvandensteen@gmail.com>
2024-07-08 08:07:12 -0700

    Should be paginated get to receive all

--- lib/greenhouse_io/api/client.rb
+++ lib/greenhouse_io/api/client.rb
@@ -14,7 +14,7 @@ module GreenhouseIo
       'job_posts' => [:active, :live],
       'users' => [:user_attributes, :email],
       'applications' => [:last_activity_after, :status, :job_id],
-      'scheduled_interviews' => [:starts_after, :starts_before, :ends_after, :ends_before],
+      'scheduled_interviews' => [:starts_after, :starts_before, :ends_after, :ends_before, :updated_after],
       'scorecards' => [:updated_after, :skip_count],
     }
 
@@ -87,7 +87,7 @@ module GreenhouseIo
     end
 
     def scheduled_interviews(options = {})
-      get_from_harvest_api "/scheduled_interviews", options, 'scheduled_interviews'
+      paginated_get "/scheduled_interviews", options, 'scheduled_interviews'
     end
 
     def interview(id, options = {})
